383eb08cbdac13c07358454d0526192e7afcb6d7
Charleen777 <pzhangl@mail2.sysu.edu.cn>
2016-01-28 13:29:02 +0800

    modified prototxt/vgg5_lstm5

--- prototxt/vgg5_lstm5/test.sh
+++ prototxt/vgg5_lstm5/test.sh
@@ -3,4 +3,4 @@
 LOG=logfile/test-`date +%Y-%m-%d-%H-%M-%S`-vgg5_lstm5.log
 CAFFE=./build/tools/caffe
 
-$CAFFE test --model=prototxt/vgg5_lstm5/test.prototxt --weights=snapshots/vgg5_lstm5_iter_$1.caffemodel --iterations=200 --gpu=3  2>&1 | tee $LOG
+$CAFFE test --model=prototxt/vgg5_lstm5/test.prototxt --weights=snapshots/vgg5_lstm5_iter_$1.caffemodel --iterations=200 --gpu=0  2>&1 | tee $LOG
